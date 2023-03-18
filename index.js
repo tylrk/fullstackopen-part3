20548d9b80e2ab34ec65d6e2b4aa920ba065c481
tylrk <tylerk1117@gmail.com>
2023-03-18 12:13:09 -0500

    completed step2

--- index.js
+++ index.js
@@ -25,7 +25,16 @@ const persons = [
 ];
 
 app.get("/", (request, response) => {
-  response.send("<h1>Wazzzuupppp</h1>");
+  response.send("<h1>Phonebook Landing Page</h1>");
+});
+
+app.get("/info", (request, response) => {
+  const date = new Date();
+
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+     <p>${date}</p>`
+  );
 });
 
 app.get("/api/persons", (request, response) => {
