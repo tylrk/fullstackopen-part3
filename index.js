240ec1ad3466432026fd637675463f7b09a5a811
tylrk <tylerk1117@gmail.com>
2023-03-19 21:55:19 -0500

    completed step8

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const morgan = require("morgan");
 
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"));
+
+morgan.token('data', (request, response) => {
+    return JSON.stringify(request.body)
+})
 
 let persons = [
   {
