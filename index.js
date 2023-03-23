916cd504267f0186476912c2e169940e28bc33c1
tylrk <tylerk1117@gmail.com>
2023-03-22 21:58:42 -0500

    completed part3.9

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"));
+app.use(cors());
 
 morgan.token('data', (request, response) => {
     return JSON.stringify(request.body)
