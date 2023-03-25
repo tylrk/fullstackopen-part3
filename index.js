524259eba72333cd0ba1691e4f5207af5c04d7cd
tylrk <tylerk1117@gmail.com>
2023-03-24 21:57:20 -0500

    added prod build

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 app.use(express.json());
+app.use(express.static('build'));
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"));
 app.use(cors());
 
