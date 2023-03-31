318976ae1fa7e3b284f97f40b98c4842a20276ea
tylrk <tylerk1117@gmail.com>
2023-03-30 19:34:31 -0500

    completed part3.12

---
+++ mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://tylerk1117:${password}@fso-phonebook.9sjgzlp.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+});
+
+if (process.argv.length > 3) {
+  person.save().then((result) => {
+    console.log(`Added ${person.name} number ${person.number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
+
+Person.find({}).then((result) => {
+  console.log("phonebook:");
+  result.forEach((person) => {
+    console.log(person.name, person.number);
+  });
+  mongoose.connection.close();
+});
